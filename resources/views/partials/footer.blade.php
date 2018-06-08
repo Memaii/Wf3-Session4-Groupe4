d62d842ee70e28b2fe2ff6d565aa29037a0a0ff6
ben891 <v2g89@hotmail.fr>
2018-06-08 14:57:20 +0200

    footertrad

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,7 +6,7 @@ $routePrefix = Route::getCurrentRoute()->getPrefix();
 				<div class="row">
 					<section class="col-md-6 col-lg-3">
 						
-						
+						<h2>Coordonnée</h2>
 						<p><i class="fas fa-map-marker-alt"></i>&nbsp;0123&nbsp;Rue, De La&nbsp;Gare, Auxerre&nbsp;89000</p>
 						<p><i class="fas fa-phone"></i>&nbsp;(+33)&nbsp;386010203</p>
 						<p><i class="fas fa-envelope"></i>&nbsp;info@psdfreebies.com</p>
@@ -14,7 +14,8 @@ $routePrefix = Route::getCurrentRoute()->getPrefix();
 					<section class="col-md-6 col-lg-3">
 						<h2>Information</h2>
 						<p>Politique & Confidentialité</p>
-						<p>Termes et Conditions</p>
+
+						<p>Mentoin Légale</p>
 						<p>Expédition & Livraison</p>
 						<p>FAQs</p>
 					</section>
@@ -38,7 +39,7 @@ $routePrefix = Route::getCurrentRoute()->getPrefix();
 					<section class="col-md-6 col-lg-3">
 
 
-						<h2>Payment Option</h2>
+						<h2>Modalité de paiement</h2>
 						<p><img src="assets/img/payment.png"></p>
 						<p><img src="assets/img/payment2.png"></p>
 					</section>		
@@ -65,7 +66,7 @@ $routePrefix = Route::getCurrentRoute()->getPrefix();
 						</li>
 
 						<li class="nav-item">
-							<a class="nav-link" href="#">Contacte</a>
+							<a class="nav-link" href="#">Contact</a>
 						</li>
 					</ul>
 				</div>

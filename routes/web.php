059715418c36c4d4b6f6e83b8fe5fbb3e79b437c
ben891 <v2g89@hotmail.fr>
2018-06-08 11:39:41 +0200

    modifrouteboutiqueid

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::prefix('admin')->group(function(){
 
 //Routes boutique
 Route::get('/boutiques', 'boutiqueController@boutiques')->middleware('auth')->name('boutiques');
-Route::get('/boutique/{slug}', 'boutiqueController@boutique')->middleware('auth')->name('boutique');
+Route::get('/boutique/{id}', 'boutiqueController@boutique')->middleware('auth')->name('boutique');
 Route::get('/boutique/ajoutCommentaire', 'boutiqueController@boutiquecomm')->middleware('auth')->name('boutiquecomm');
 
 

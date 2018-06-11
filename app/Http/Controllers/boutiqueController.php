59886fa4c77673a5686c8f730bfe2f703c5ae5e6
CHuart <christine.huart61@gmail.com>
2018-06-11 16:13:31 +0200

    boutiques

--- app/Http/Controllers/boutiqueController.php
+++ app/Http/Controllers/boutiqueController.php
@@ -4,19 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\shopModel as Shops;
+use App\listeboutiquesModel as Shops;
+use App\shopModel as Shop;
 use App\opinionModel as Opinion;
-use App\productModel as Pruducts;
+use App\productModel as Products;
 
 class boutiqueController extends Controller
 {
     public function boutique($id){
     	// ici on récupère les données de la boutique
     	$shop = Shops::where('id_shop', $id)->first();
-    	// ici on récupère les avis sur la boutique
 
     	// ici on récupère les produits de la boutique
-    	$products = Pruducts::where('shop_id_shop', $id)->paginate(9);
+    	$products = Products::where('shop_id_shop', $id)->paginate(9);
 
     return view('boutique', ['shop' => $shop, 'products' => $products]);
 	}
--- app/Http/Controllers/indexController.php
+++ app/Http/Controllers/indexController.php
@@ -4,21 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\shopModel as Shops;
-use App\opinionModel as Opinion;
+use App\listeboutiquesModel as Shops;
+
 
 class indexController extends Controller
 {
     public function accueil(){
     	$shops = Shops::paginate(10);
 
-    	$note = Opinion::where('type_opinion', '1')->avg('note_opinion');
-/*
-	    	$note = Opinion::whereColunm([
-    						['type_opinion', '=', 1],
-    						['type_id', '=', 1 ]
-    					])->get();
-*/
-    	return view('welcome', ['shops' => $shops, 'note' => $note]);
+    	return view('welcome', ['shops' => $shops]);
     }
 }

9e0948b46e9ddc76a81bc55d23b59fe7d0094a1a
CHuart <christine.huart61@gmail.com>
2018-06-11 09:38:32 +0200

    boutique

--- app/Http/Controllers/indexController.php
+++ app/Http/Controllers/indexController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\shopModel as Shops;
+use App\opinionModel as Opinion;
+
 class indexController extends Controller
 {
     public function accueil(){
-    
-    	$titre = "Page d'accueil HTEV";
-    	return view('welcome', ['titre' => $titre]);
+    	$shops = Shops::paginate(10);
+
+    	$note = Opinion::where('type_opinion', '1')->avg('note_opinion');
+/*
+	    	$note = Opinion::whereColunm([
+    						['type_opinion', '=', 1],
+    						['type_id', '=', 1 ]
+    					])->get();
+*/
+    	return view('welcome', ['shops' => $shops, 'note' => $note]);
     }
 }

0ad22f9d6421d6eeec1e734e471aabe0e9e4732f
jumutu <jumutu@gmail.com>
2018-06-14 10:46:45 +0200

    ajout et modif
    ajout de boutiques.blade
    modif de boutiquecontroller
    
    ps : ya pas la carte

--- app/Http/Controllers/boutiqueController.php
+++ app/Http/Controllers/boutiqueController.php
@@ -11,13 +11,20 @@ use App\productModel as Products;
 
 class boutiqueController extends Controller
 {
-    public function boutique($id){
-    	// ici on récupère les données de la boutique
-    	$shop = Shops::where('id_shop', $id)->first();
+	public function boutiques(){
+		// ici on récupère les données des boutiques
+		$shops = Shops::where('statut_shop',2)->orderby('name_shop')->paginate(9);
 
-    	// ici on récupère les produits de la boutique
-    	$products = Products::where('shop_id_shop', $id)->paginate(9);
+	return view('boutiques', ['shops' => $shops,]);
+	}
 
-    return view('boutique', ['shop' => $shop, 'products' => $products]);
+	public function boutique($id){
+		// ici on récupère les données de la boutique
+		$shop = Shops::where('id_shop', $id)->first();
+
+		// ici on récupère les produits de la boutique
+		$products = Products::where('shop_id_shop', $id)->paginate(9);
+
+	return view('boutique', ['shop' => $shop, 'products' => $products]);
 	}
 }

3e0c284da182b2a2b90a229da5c2dbea6859780d
Blackliss <blackliss89@gmail.com>
2018-06-14 15:22:13 +0200

    Poire

--- resources/views/boutiques.blade.php
+++ resources/views/boutiques.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.mainlayout')
 
-@section('title', "Accueil")
+@section('title', "boutiques")
 
 @section('content')
 
@@ -9,16 +9,18 @@
 		<section class="col-9">
 			<div class="card-deck">
                 <div class="card-columns">
+
+                    {{-- affichage des boutiques --}}
                     @foreach($shops as $shop)
                     <div class="card p-2">
-                        <!-- affichage de l'image-->
-                        <a href="{{ route('boutique',['slugb' => $shop->slug_shop]) }}">
-                         <img class="w-100" src="{{ asset('assets/img/uploads/featured') }}/{{ $shop->link_logo }}" alt="{{ $shop->name_shop }}"> 
-                     </a>
+                       {{-- affichage de l'image --}}
+                       <a href="{{route('boutique',['id' => $shop->id_shop])}}">
+                        <img class="w-100" src="{{ asset('assets/img/uploads/featured') }}/{{ $shop->link_logo }}" alt="{{ $shop->name_shop  }}"> 
+                    </a>
 
-                     <div class="card-body">
-                        <h2 class="card-title text-center">
-                            <a href="{{ route('boutique',['slugb' => $shop->slug_shop]) }}">{{ $shop->name_shop }}</a>
+                    <div class="card-body">
+                        <h2 class="card-title">
+                            <a href="{{route('boutique',['id' => $shop->id_shop])}}">{{ $shop->name_shop }}</a>
                         </h2>
                         <p>{!! strip_tags(str_limit($shop ->description, $limit=150, $end='...')) !!}</p>
                         <p>note:</p>
@@ -33,11 +35,13 @@
                   </div>
               </div>
               @endforeach
-          </section>
-          <aside class="col-3">
 
-          </aside>
-      </div>
-  </main>
+          </div>
+      </section>
+      <aside class="col-3">
+          
+      </aside>
+  </div>
+</main>
 
-  @endsection
+@endsection
